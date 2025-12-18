0990375357ec169367d3fbf1252bb42236bc4b1c
Gaurav Karade <143058722+Gavi08era@users.noreply.github.com>
2025-12-18 22:25:56 +0530

    Add files via upload

---
+++ dictHog.py
@@ -0,0 +1,9 @@
+students={
+    "Hermoine":"Gryffindor",
+    "Harry":"Gryffindor",
+    "Ron":"Gryffindor",
+    "Draco":"Slythrin"
+    }
+
+for i in students:
+    print(i, students[i], sep=", ")
